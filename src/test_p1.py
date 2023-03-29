f116f8c4967b9339b8fc9bdc1ad57ea72cb1d1f3
Santinu COmiti <santinu.comiti@bordeaux-inp.fr>
2023-03-29 19:31:57 +0200

    adding tests to compare raphson and raphson backtrack ...

--- src/test_p1.py
+++ src/test_p1.py
@@ -17,33 +17,21 @@ def deri(x):
 
 def function2(x):
     return x**3-x**2
-    
+
+
 def deri2(x):
     return 3*x**2-2*x
 
-function()
-
-start,eps,N=-1.0,0.01,100
-eps2=0.01
+start,eps,N=1.1,0.01,100
+eps2=0.75
 print("Solution de la fonction f:x-> x**2 - 4 avec U0="+str(start)+" epsilon = "+str(eps)+" : "+str(Newton_Raphson(function,deri,start,N,eps)))
 print("Solution de la fonction f:x-> x**3-x**2 avec U0="+str(start)+" epsilon = "+str(eps)+" : "+str(Newton_Raphson(function2,deri2,start,N,eps)))
+
+
 
 time.sleep(0.25)
 print("")
 
-def function_2(x,alpha):
-    return x**alpha
-def deri3(x,alpha):
-    return alpha*(x**(alpha-1))
-
-alph=100
-
-#for i in range(10,1000):
-#    assert()
-print("fonction dont on ne trouve pas la solution avec Newton classique : f(x)-> 1-x**"+str(alph))
-print("la solution selon Newton Raphson est : ")
-print(Newton_Raphson(function,deri,start,N,eps))
-print(" alors que la solution théorique est 1...")
 
 print("Testing matrixes...")
 print("")
@@ -64,8 +52,8 @@ time.sleep(0.25)
 print("Solving f(u,v)-> ((u+v)/2,(u-v)/2)=(0,0)")
 print("starting point : [1,1]")
 start_mat=np.ones(2)
-print("Vecteur solution : ")
-print(Newton_Raphson(f_mat_1,J_1,start_mat,100,eps))
+print("Vector solution : ")
+print(Newton_Raphson(f_mat_1,J_1,start_mat,N,eps))
 print("")
 time.sleep(0.25)
 
@@ -83,7 +71,7 @@ def J_2(Xn):
 print("Solving f(u,v)-> ((u+v)**2/2-5,(u-v)**2/2)=(0,0)")
 print("starting point : [1,1]")
 start_mat=np.ones(2)
-print("Vecteur solution : ")
+print("Vector solution : ")
 print(Newton_Raphson(f_mat_2,J_2,start_mat,100,eps))
 
 
@@ -93,6 +81,36 @@ print(Newton_Raphson(f_mat_2,J_2,start_mat,100,eps))
 
 
 #QUESTION 4
+print("")
+print("QUESTION 4...")
+time.sleep(0.5)
+print("")
+
+
+alph=1
+def function3(x):
+    return x**alph
+def deri3(x):
+    return alph*(x**(alph-1))
+
+liste=[]
+
+for i in range(1,250):
+    if abs(Newton_Raphson(function3,deri3,start,N,eps))>(10*eps):
+        liste.append(alph)
+        break
+    alph+=1
+print("With the following parameters N="+str(N)+" epsilon="+str(eps)+" starting point x0="+str(start) )
+print("minimum exponent that makes error = 10*epsilon is : ",min(liste))
+print("The function that isn't found with Newton Raphson : f(x)-> 1-x**"+str(alph))
+print("The solution proposed by Newton Raphson is : ")
+print(Newton_Raphson(function3,deri3,start,N,eps))
+print(" In theory, the solution is 0...")
+
+print("")
+print("Compared to Newton_raphson_bactrack : ")
+print(Newton_Raphson_backtrack(function3,deri3,start,N,eps,eps2))
+
 
 #print("Solution de la fonction f:x-> x**2 avec U0=",start,"epsilon = ",epsilon," : ","epsilon2 ( bactracking) = ",Newton_Raphson_backtrack(function,deri,start,N,eps,eps2))
 #print("Solution de la fonction f:x-> x**3-x**2 avec U0=",start,"epsilon = ",epsilon,"epsilon2 ( bactracking) = ",eps2," : ",Newton_Raphson_backtrack(function2,deri2,start,eps,eps2))
