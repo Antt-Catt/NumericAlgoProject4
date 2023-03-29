887b2a01313c5ac9004ea4d20e5c3eea0747ce79
COMITI Santinu <santinu.comiti@enseirb-matmeca.fr>
2023-03-29 18:02:21 +0200

    Adding tests...

--- src/part_1.py
+++ src/part_1.py
@@ -22,15 +22,22 @@ def Newton_Raphson(f,J,U0,N,epsilon):
 
 
 def Newton_Raphson_backtrack(f,J,U0,N,epsilon,eps2):
+
     Xn=U0
-    alpha=1
+    if (type(U0)==float)or (type(U0)==int):
+        for i in range(N):
+            Xn=Xn-f(Xn)/J(Xn)
+            if abs(Xn)<epsilon:
+                return Xn
+        return Xn
+
     for i in range(N):
-        h=np.linalg.lstsq(J,f(Xn))
+        h=np.linalg.lstsq(J(Xn),f(Xn))
         while np.linalg.norm(f(Xn-alpha*h))-np.linalg.norm(f(Xn))>0:
             alpha=alpha*0.5
             if alpha<eps2:
                 return False
-        Xn=np.substract(Xn,h)
+        Xn=np.subtract(Xn,h[0])
         if np.linalg.norm(Xn)<epsilon:
             return Xn
     return Xn
